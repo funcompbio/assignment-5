e514a20b207793bfa7af02a9296b3e79879bdc62
Robert Castelo <robert.castelo@upf.edu>
2020-10-30 20:20:10 +0100

    Replaced identation spaces by tabs in src/sir.py and src/sirv.py.

--- src/sir.py
+++ src/sir.py
@@ -25,33 +25,33 @@ import numpy as np
 ## set of time points. DO NOT EDIT THE NEXT LINE
 def main(t, rho, alpha) :
 
-  ## initial value
-  ## DO NOT EDIT THE NEXT LINE
-  I0 = 0.001
+	## DO NOT EDIT THE NEXT TWO LINES
+	## initial value
+	I0 = 0.001
 
-  ## REPLACE THIS AND THE NEXT TWO LINES
-  ## by a proper call to the 'euler()' function
-  I = t
+	## REPLACE THIS AND THE NEXT TWO LINES
+	## by a proper call to the 'euler()' function
+	I = t
 
-  ## DO NOT EDIT THE NEXT LINE
-  return(I)
+	## DO NOT EDIT THE NEXT LINE
+	return(I)
 
 ## DO NOT EDIT THESE LINES TILL THE END OF THE SCRIPT
 if __name__ == "__main__" :
-    import sys
+	import sys
 
-    if (len(sys.argv) != 3) :
-        print("error: sir.py <rho> <alpha>")
-    else :
-        ## set time points between 0 and 30 in steps (h) of 1
-        t = np.arange(0, 30, 1)
+	if (len(sys.argv) != 3) :
+		print("error: sir.py <rho> <alpha>")
+	else :
+		## set time points between 0 and 30 in steps (h) of 1
+		t = np.arange(0, 30, 1)
 
-        ## call the 'main()' function
-        I = main(t, float(sys.argv[1]), float(sys.argv[2]))
+		## call the 'main()' function
+		I = main(t, float(sys.argv[1]), float(sys.argv[2]))
 
-        ## print resulting values for each time point
-        print("t,dI")
-        i = 0
-        while (i < len(I)) :
-          print("%.3f,%.3f" %(t[i], I[i]))
-          i = i + 1
+		## print resulting values for each time point
+		print("t,dI")
+		i = 0
+		while (i < len(I)) :
+			print("%.3f,%.3f" %(t[i], I[i]))
+			i = i + 1
